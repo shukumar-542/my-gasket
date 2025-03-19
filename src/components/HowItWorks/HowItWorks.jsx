ee76be702209d940d8049d1632cd8e52f54e4512
shukumar-542 <shukumar542@gmail.com>
2025-03-19 18:01:23 +0600

    Fix how it work section

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,8 +1,20 @@
+import { time } from 'console'
 import React from 'react'
+
+const HIW = [
+    {
+        no : "01",
+        title : "Title 01" 
+    }
+]
+
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <div>
+        <h1 className='text-[48px] font-extrabold text-center uppercase'>How It Works</h1>
+        <p className='text-[16px] text-[#686868] text-center'>We break down the process into simple steps to ensure seamless execution, from start to finish.</p>
+    </div>
   )
 }
 
