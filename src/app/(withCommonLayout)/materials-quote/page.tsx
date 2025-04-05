0d620fa87ff572e030f255a83e92ac2b8c7e0b45
shukumar-542 <shukumar542@gmail.com>
2025-04-05 12:43:17 +0600

    Implement file uploader

--- src/app/(withCommonLayout)/materials-quote/page.tsx
+++ src/app/(withCommonLayout)/materials-quote/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import img from "../../../assets/quote.png";
-import { AiTwotoneInfoCircle } from "react-icons/ai";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import WorkProcess from "@/components/WorkProcess/WorkProcess";
+import { HiOutlineUpload } from "react-icons/hi";
 const MaterialsQuotePage = () => {
+  const [file, setFile] = useState<File | null>(null);
   const workSteps = [
     {
       step: "01",
@@ -24,10 +26,28 @@ const MaterialsQuotePage = () => {
       title: "It must have a top view only.",
     },
   ];
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+
+    if (selectedFile) {
+      const allowedExtensions = [".dxf", ".dwg"];
+      const fileExtension = selectedFile.name
+        .slice(selectedFile.name.lastIndexOf("."))
+        .toLowerCase();
+
+      if (allowedExtensions.includes(fileExtension)) {
+        setFile(selectedFile);
+        console.log("Accepted file:", selectedFile);
+      } else {
+        alert("Only .dxf or .dwg files are allowed");
+        e.target.value = "";
+      }
+    }
+  };
+
   return (
-    <div className=" mt-20 ">
-
-
+    <div className=" mt-20  ">
       <div className="mt-8 relative ">
         <Image
           className="w-full h-[400px] object-cover"
@@ -42,14 +62,39 @@ const MaterialsQuotePage = () => {
         </p>
       </div>
 
-        <div className="flex items-center justify-center py-10 gap-2  bg-white px-2 md:px-0">
-            <p className=" font-semibold text-[25px] md:text-[38px]">How does the quote generator work? </p>
-            <p className="text-[#F97316] cursor-pointer"><IoMdInformationCircleOutline size={35}  /></p>
-        </div>
-        <div className="bg-white mx-2 md:mx-0">
-            <WorkProcess workSteps={workSteps}/>
-        </div>
+      <div className="flex items-center justify-center py-10 gap-2  bg-white px-2 md:px-0">
+        <p className=" font-semibold text-[25px] md:text-[38px]">
+          How does the quote generator work?{" "}
+        </p>
+        <p className="text-[#F97316] cursor-pointer">
+          <IoMdInformationCircleOutline size={35} />
+        </p>
+      </div>
+      <div className="bg-white mx-2 md:mx-0">
+        <WorkProcess workSteps={workSteps} />
+      </div>
 
+      <div className="py-10 container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div>
+          <label
+            htmlFor="file-upload"
+            className="flex flex-col items-center justify-center border-2 border-dashed border-blue-300 rounded-xl p-6 cursor-pointer bg-blue-50 hover:bg-blue-100 transition"
+          >
+            <HiOutlineUpload size={50} />
+            <input
+              id="file-upload"
+              type="file"
+              accept=".dxf,.dwg"
+              onChange={handleFileChange}
+              className="hidden"
+            />
+            {file ? <p className="mt-4 text-[#F97316] text-center">
+              {file.name}
+            </p> : "Upload File"}
+          </label>
+          
+        </div>
+      </div>
     </div>
   );
 };
