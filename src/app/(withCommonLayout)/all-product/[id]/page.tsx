e07fd50138b2a67c1c7bd02a39e6ec1780a50f51
shukumar-542 <shukumar542@gmail.com>
2025-04-07 11:19:01 +0600

    Implement main feature detais

--- src/app/(withCommonLayout)/all-product/[id]/page.tsx
+++ src/app/(withCommonLayout)/all-product/[id]/page.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import hero from "../../../../assets/detailsHero.png";
 import img from "../../../../assets/details1.png";
 import Image from "next/image";
-import arrowGreen from '../../../../assets/arrow-green.png'
-import arrowBlue from '../../../../assets/arrow-blue.png'
-import arrowRed from '../../../../assets/arrow-red.png'
-
+import arrowGreen from "../../../../assets/arrow-green.png";
+import arrowBlue from "../../../../assets/arrow-blue.png";
+import arrowRed from "../../../../assets/arrow-red.png";
+import details1 from "../../../../assets/details2.png";
+import details2 from "../../../../assets/details3.png";
+import details3 from "../../../../assets/details4.png";
+import FeatureCard from "@/components/FeatureCard/FeatureCard";
 
 const ProductDetailsPage = () => {
   return (
@@ -43,62 +46,157 @@ const ProductDetailsPage = () => {
         </div>
       </div>
 
-
       {/* Feature sections */}
 
       <div className="container mx-auto">
-
         <div className="grid grid-cols-1 md:grid-cols-3 justify-between gap-5 py-16">
           <div className="w-full">
             <h1 className="text-[#16A34A] text-[38px] font-bold mb-10">PROS</h1>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowGreen} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowGreen}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">High-Temperature Resistance</p>
             </div>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowGreen} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowGreen}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Superior Sealing Performance</p>
             </div>
             <div className="flex items-center gap-2">
-              <Image  src={arrowGreen} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowGreen}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Long-Lasting & Durable</p>
             </div>
           </div>
-
 
           <div className="w-full">
             <h1 className="text-red-500 text-[38px] font-bold mb-10">CONS</h1>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowRed} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowRed}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">High-Temperature Resistance</p>
             </div>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowRed} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowRed}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Superior Sealing Performance</p>
             </div>
             <div className="flex items-center gap-2">
-              <Image  src={arrowRed} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowRed}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Long-Lasting & Durable</p>
             </div>
           </div>
 
           <div className="w-full">
-            <h1 className="text-[#3B82F6] text-[38px] font-bold mb-10">MAIN FEATURES</h1>
+            <h1 className="text-[#3B82F6] text-[38px] font-bold mb-10">
+              MAIN FEATURES
+            </h1>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowBlue} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowBlue}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">High-Temperature Resistance</p>
             </div>
             <div className="flex items-center gap-2 mb-2">
-              <Image  src={arrowBlue} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowBlue}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Superior Sealing Performance</p>
             </div>
             <div className="flex items-center gap-2">
-              <Image  src={arrowBlue} height={600} width={600} className="h-6 w-6" alt="img" />
+              <Image
+                src={arrowBlue}
+                height={600}
+                width={600}
+                className="h-6 w-6"
+                alt="img"
+              />
               <p className="text-xl">Long-Lasting & Durable</p>
             </div>
           </div>
         </div>
 
+        {/* main feature details */}
+
+        <FeatureCard image={details1} 
+        title={" High-Temperature Resistance"}
+        description ="Designed to endure extreme heat without compromising performance, making it ideal for automotive engines, industrial machinery, and high-pressure applications."
+        borderColor="#20B7CC"
+        />
+        <FeatureCard image={details2} 
+        title={" High-Temperature Resistance"}
+        description ="Designed to endure extreme heat without compromising performance, making it ideal for automotive engines, industrial machinery, and high-pressure applications."
+         borderColor="#F97316"
+        reverse ={true}
+        />
+        <FeatureCard image={details3} 
+        title={" High-Temperature Resistance"}
+        description ="Designed to endure extreme heat without compromising performance, making it ideal for automotive engines, industrial machinery, and high-pressure applications."
+         borderColor="#20B7CC"
+        />
+        {/* <FeatureCard image={details2} /> */}
+
+        {/* <div className="md:flex items-center py-20">
+          <div className="w-full">
+            <Image
+              src={details1}
+              height={600}
+              width={600}
+              className=""
+              alt="img"
+            />
+          </div>
+          <div className="w-full">
+            <p className="text-[32px] font-extrabold pb-5">
+              High-Temperature Resistance
+            </p>
+            <div className="border-3 max-w-[25%] border-[#20B7CC] "></div>
+            <p className="pt-5 text-[#475569] text-[18px]">
+              Designed to endure extreme heat without compromising performance,
+              making it ideal for automotive engines, industrial machinery, and
+              high-pressure applications.
+            </p>
+          </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,41 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+
+
+type FeatureCardProps = {
+  image : StaticImageData,
+  title : string,
+  description : string,
+  reverse?: boolean;
+  borderColor : string
+}
+const FeatureCard : React.FC<FeatureCardProps> = ({image , title , description ,reverse = false, borderColor}) => {
+  return (
+    <div className={
+      `md:flex items-center gap-20 py-10 ${
+        reverse ? 'flex-row-reverse' : 'flex-row'}
+      `
+    }>
+          <div className="w-full ">
+            <Image
+              src={image}
+              height={600}
+              width={600}
+              className="w-full"
+              alt="img"
+            />
+          </div>
+          <div className="w-full">
+            <p className="text-[32px] font-extrabold pb-5">
+              {title}
+            </p>
+            <div className="border-3 max-w-[25%]  "  style={{ borderColor, borderStyle: 'solid' }}></div>
+            <p className="pt-5 text-[#475569] text-[18px]">
+              {description}
+            </p>
+          </div>
+        </div>
+  )
+}
+
+export default FeatureCard
