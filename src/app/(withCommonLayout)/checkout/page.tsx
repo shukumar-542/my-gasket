a996491e3444e988e3da1b6311e9ccaa995e7d5b
shukumar-542 <shukumar542@gmail.com>
2025-04-19 07:55:06 +0600

    Implement conditionally show the checkout page

--- src/app/(withCommonLayout)/checkout/page.tsx
+++ src/app/(withCommonLayout)/checkout/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Select } from "antd";
+import { Input, Select } from "antd";
 import React, { useState } from "react";
 import img from "../../../assets/product2.png";
 import Image from "next/image";
+import TextArea from "antd/es/input/TextArea";
 const CheckOutPage = () => {
   const [businessType, setBusinessType] = useState("business");
   const handleChange = (value: string) => {
@@ -28,6 +29,71 @@ const CheckOutPage = () => {
                 { value: "business", label: "Business" },
                 { value: "personal", label: "Personal" },
               ]}
+            />
+          </div>
+
+          {businessType == "business" ? (
+            <div>
+              <p className="mt-5 mb-1">VAT Number</p>
+              <Input
+                rootClassName="custom-input"
+                placeholder="Enter your VAT number"
+              />
+            </div>
+          ) : (
+            <div>
+              <p className="mt-5 mb-1">Full Name</p>
+              <Input
+                rootClassName="custom-input"
+                placeholder="Enter your Full Name"
+              />
+            </div>
+          )}
+
+          {
+            businessType == "business" ?  <div>
+            <p className="mt-5 mb-1">VAT Number</p>
+            <Input
+              rootClassName="custom-input"
+              placeholder="Enter your VAT number"
+            />
+          </div> : <div>
+            <p className="mt-5 mb-1">Social Security Number</p>
+            <Input
+              rootClassName="custom-input"
+              placeholder="Enter your Social Security Number"
+            />
+          </div> 
+          }
+
+      
+          <div>
+            <p className="mt-5 mb-1">Email</p>
+            <Input
+              rootClassName="custom-input"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div>
+            <p className="mt-5 mb-1">Phone</p>
+            <Input
+              rootClassName="custom-input"
+              placeholder="Enter your phone number"
+            />
+          </div>
+          <div>
+            <p className="mt-5 mb-1">Address</p>
+            <Input
+              rootClassName="custom-input"
+              placeholder="Enter your Address"
+            />
+          </div>
+          <div>
+            <p className="mt-5 mb-1">Additional Note</p>
+            <TextArea
+              rows={4}
+              rootClassName="custom-input"
+              placeholder="Additional Text"
             />
           </div>
         </div>
@@ -95,6 +161,29 @@ const CheckOutPage = () => {
               <p>€10.85</p>
             </div>
           </div>
+
+          {/* Payment card */}
+          <div className="shadow-2xl rounded-md  mt-5">
+            <div className="flex justify-between bg-[#E5E7EB] py-3 rounded-tl-md  rounded-tr-md text-xl font-semibold p-2">
+              <p>Payment </p>
+              <p>Subtotal</p>
+            </div>
+            <label className="flex items-center space-x-2 cursor-pointer p-5">
+              <input
+                type="radio"
+                name="paymentMethod"
+                value="bankTransfer"
+                className="form-radio text-blue-600 focus:ring-blue-500"
+                // defaultChecked
+              />
+              <span className="text-gray-800">Bank Transfer</span>
+            </label>
+          </div>
+
+          {/* Checkout button */}
+          <button className="bg-[#F97316] w-full mt-5 rounded-sm text-white py-2 text-[20px] shadow-2xl">
+            Place an Order
+          </button>
         </div>
       </div>
     </div>
